2fa925b651a59d522d0b33cde1a2421b843b9abe
Fredrik Nygård Carlsen <fnc@Fredriks-MacBook-Pro.local>
2011-08-06 03:00:20 +0200

    laget støtte for å createFolder

--- src/FTPStorage.java
+++ src/FTPStorage.java
@@ -8,16 +8,12 @@ import it.sauronsoftware.ftp4j.FTPIllegalReplyException;
 import java.io.IOException;
 
 public class FTPStorage {
-
 	FTPClient client = new FTPClient();
 
-
 	public FTPStorage() {
-
 		try {
 			client.connect("backup.fncit.no");
 			client.login("frecar", "76ahf6234a!!");
-
 		} catch (IllegalStateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -51,23 +47,35 @@ public class FTPStorage {
 	public void createFolder(String folder) {
 
 		try {
-			this.client.changeDirectory(folder);
-			FTPFile[] list = this.client.list();
-
-			for(FTPFile file : list) {
-				System.out.print(file);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+			this.client.changeDirectory("~/");
+		} catch (Exception e2) {
+			e2.printStackTrace();
 		}	
-	}
 
+		String[] path = folder.split("/");
+		String mid_path = "";
+
+		for(String p : path) {
+			mid_path+=p+"/";
+			
+			try {
+				this.client.changeDirectory(mid_path);	
+			}
+			catch(Exception e) {
+				try {
+					this.client.createDirectory(mid_path);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}			
+			}
+		}
+	}
 
 
 	public void upload(String destination, String local_file) {
 
 		try {
-			this.client.createDirectory(destination);
+			this.createFolder(destination);
 			this.client.changeDirectory(destination);
 			this.client.upload(new java.io.File(local_file));
 
